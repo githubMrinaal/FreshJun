c44559b3946bc46f311324050b428031fdb48056
Sumedh-Joshi <sumedhjoshiglobal4@gmail.com>
2025-02-15 19:09:14 -0500

    stretch remains

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-var map = L.map('map').setView([0, 0], 2);
+var map = L.map('map').setView([0, 0], 1);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    noWrap: true // This line prevents the map from wrapping around horizontally.
 }).addTo(map);
 
 // Placeholder data for countries
@@ -75,3 +76,4 @@ fetch("countries.geojson")
     .then(data => {
         geojson.addData(data);
     });
+
